a2e8f1cab92254682bc830d5e4d9500c827404bc
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-26 02:51:16 +0530

    Update CategoryWomen.js

--- src/Components/CategoryWomen.js
+++ src/Components/CategoryWomen.js
@@ -7,9 +7,7 @@ export default function CategoryWomen(){
             
             <div key={items.id} className=' m-3 lg:flex lg:flex-col justify-center relative'>
             <img  src={items.img} className={`rounded-2xl hover:scale-100 transition ease-in-out duration-500 h-96 w-96 scale-90 cursor-pointer`} />
-            <div className="absolute inset-0 flex items- justify-center opacity-0 hover:opacity-100 scale-90 hover:scale-100  transition-opacity">
-                <p className="text-white font-bold">{items.type}</p>
-            </div>
+           
             </div>
         )
     })
