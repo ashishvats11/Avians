b0caaa1c83bde0c4a368999a4edf6a7cf32ebc2b
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-26 02:37:41 +0530

    Update nav.js

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,11 +1,12 @@
 import {Routes,Route,Link} from 'react-router-dom';
 import Footer from './Footer';
+
 function Nav(){
     return(
         <div className="flex justify-between bg-teal-600 h-10  " >
             <ul className="flex items-center mx-9 ">
-                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">Home </li>
-                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8"> About</li>
+                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">Home</li>
+                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">About</li>
                 <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">Blog</li>
                 <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">Services</li>
             </ul>
