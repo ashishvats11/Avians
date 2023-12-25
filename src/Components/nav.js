77ad9b71580f4b28b1aa4bf7488365fbf031eb7f
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-25 23:52:15 +0530

    Update nav.js

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,12 +1,16 @@
+import {Routes,Route,Link} from 'react-router-dom';
+import Footer from './Footer';
 function Nav(){
     return(
-        <div class="flex justify-end " >
-            <ul class="flex space-x-6 mx-9">
-                <li class="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500 hover:text-lg ">Home</li>
-                <li class="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500 hover:text-lg"> About</li>
-                <li class="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500 hover:text-lg">Blog</li>
-                <li class="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500 hover:text-lg">Services</li>
+        <div className="flex justify-between bg-teal-600 h-10  " >
+            <ul className="flex items-center mx-9 ">
+                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">Home </li>
+                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8"> About</li>
+                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">Blog</li>
+                <li className="text-white cursor-pointer hover:underline hover:underline-offset-2 hover: decoration-orange-500  px-8">Services</li>
             </ul>
+            <button className='text-white cursor-pointer mr-10 hover:underline hover:underline-offset-2 hover:decoration-orange-500'>Login</button>
+         
         </div>
     );
 }
