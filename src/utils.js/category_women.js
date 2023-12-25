baf3a00bc37c1ec248e6be47a5e44dd6b2f7100c
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-26 02:37:48 +0530

    Create category_women.js

---
+++ src/utils.js/category_women.js
@@ -0,0 +1,25 @@
+const women =[
+    {
+        id:'1',
+        type:'Formals',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg'),
+        alt:'Formal clothing'
+    },
+    {
+        id:'2',
+        type:'Hoodies',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg')
+    },
+    {
+        id:'3',
+        type:'Oversized',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg')
+    },
+    {
+        id:'4',
+        type:'Sneakers',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg')
+    },
+    
+]
+module.exports = women;
