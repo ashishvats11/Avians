20b9c89f928917944eae8a860051e49396bbfed7
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-25 23:53:27 +0530

    category data

---
+++ src/utils.js/category.js
@@ -0,0 +1,24 @@
+const Category =[
+    {
+        id:'1',
+        type:'Formals',
+        img: require('../Assets/category/men/formals/men_formals_1.jpg'),
+        alt:'Formal clothing'
+    },
+    {
+        id:'2',
+        type:'Hoodies',
+        img:require('../Assets/category/men/hoodie/hoodie10.jpeg')
+    },
+    {
+        id:'3',
+        type:'Oversized',
+        img:require('../Assets/category/men/oversized/men_oversized_1.jpeg')
+    },
+    {
+        id:'4',
+        type:'Sneakers',
+        img:require('../Assets/category/men/sneakers/s1.jpeg')
+    },
+]
+module.exports=Category;
