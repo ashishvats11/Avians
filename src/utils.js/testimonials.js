e8fc60f6ee402bb56b01b0a04f680c6136b5c6a5
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-25 23:53:19 +0530

    testimonial data

---
+++ src/utils.js/testimonials.js
@@ -0,0 +1,61 @@
+const testimonials =
+[
+    {
+        "id":"1",
+        "remark": "Must Try!",
+        "testimonial": "This calendar app has been a lifesaver! I used to forget important events, but now I'm always on top of my schedule.",
+        "author": "Adam More",
+        "img": "https://framerusercontent.com/images/lKaaStgbmdIe8TfrIkFlKzHazV0.png"
+    },
+    {
+        "id":"2",
+        "remark": "Highly recommend!",
+        "testimonial": "This app has saved me so much time and stress! I used to constantly forget important dates, but now I can stay on top of everything. You should test it!",
+        "author": "Sophie Devilan",
+        "img": "https://framerusercontent.com/images/BRu6MhHhp9NjldqU9TTCEAEoGKg.png"
+    },
+    {   "id": "3" ,
+        "remark": "The best time manager app", 
+        "testimonial": "I love how user-friendly this app is! It's so easy to add events and set reminders, and it's made my life so much more organized.",
+        "author": "Brad de Costa",
+        "img":"https://framerusercontent.com/images/CPGgYEBeFy4gDXe5dDzh1qjQG1w.png"
+    },
+    {   "id": "4" ,
+        "remark": "Saves me time",
+        "testimonial": "Since I started using this app, I've saved so much time. I no longer have to manually add appointments or set reminders - it's all done for me!",
+        "author": "Kyle Conord ",
+        "img":"https://framerusercontent.com/images/1OwzdZHx9ElDw1ZAavs6WGWBSw.png"
+    },
+    {   "id": "5" ,
+        "remark": "Better than the rests",
+        "testimonial": "I've tried a lot of calendar apps, but this one is by far the best! It's so intuitive and customizable, and it has all the features I need. Game changer!",
+        "author": "Annie Devy",
+        "img":"https://framerusercontent.com/images/BRu6MhHhp9NjldqU9TTCEAEoGKg.png"
+    },
+    {   "id": "6" ,
+        "remark": "No more forgotten appointments",
+        "testimonial": "I'm not the most organized person, but this app has made it so easy for me to stay on top of things! I love how I can set reminders and get notifications, and it's really helped me.",
+        "author": "Michel O Neil",
+        "img":"https://framerusercontent.com/images/CPGgYEBeFy4gDXe5dDzh1qjQG1w.png"
+    },
+    {   "id": "7" ,
+        "remark": "Life-changing",
+        "testimonial": "This app is so simple yet so powerful! I love how I can easily add events and see my entire schedule at a glance.",
+        "author": "Smaira",
+        "img":"https://framerusercontent.com/images/1OwzdZHx9ElDw1ZAavs6WGWBSw.png"
+    },
+    {   "id": "8" ,
+        "remark": "Incredible UI",
+        "testimonial": "The UI of this app is just awesome, so fluid and well designed. The app has also all the features I need for my daily work. Just try it, you will fall in love!",
+        "author": "Jordan Brian",
+        "img": "https://framerusercontent.com/images/lKaaStgbmdIe8TfrIkFlKzHazV0.png"
+    },
+    {   "id": "9" ,
+        "remark": "Great for teamwork",
+        "testimonial": "If you're looking for a reliable and easy-to-use calendar app, look no further! This app has everything you need, and it's made my life so much easier",
+        "author": "Hanna Becker",
+        "img":"https://framerusercontent.com/images/1OwzdZHx9ElDw1ZAavs6WGWBSw.png"
+    }
+]
+
+module.exports=testimonials;
