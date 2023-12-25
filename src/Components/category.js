d2f4dbbdb69154bd4b5bff7ec7cd0384c8f01436
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-25 23:51:47 +0530

    Category componenet

--- src/Components/category.js
+++ src/Components/category.js
@@ -1,8 +1,24 @@
-function cards(){
+import category from '../utils.js/category'
+export default function Category(){
+    const cat_items= category.map(items=>{
+        return (
+            
+            <div key={items.id} className=' m-3 lg:flex lg:flex-col justify-center'>
+            <img  src={items.img} className={`rounded-2xl hover:scale-100 transition ease-in-out duration-500 h-96 w-96 scale-90 `} />
+            </div>
+        )
+    })
+    
     return(
 
-        <div>
-            
+        <div className='max-h-screen p-2 '>
+            <div className='flex justify-center flex-wrap'>
+                <p className='inline italic font-serif font-semibold'>Discover Your Style Symphony at AVIANS: Where Fashion Takes Flight Across Distinct Categories!</p>
+            </div>
+            <div className='flex sm:flex-row  '>
+                {cat_items}
+            </div>
+        
         </div>
 
     );
